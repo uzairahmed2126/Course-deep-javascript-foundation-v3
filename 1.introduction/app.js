40dd45e0a6fd388b7f4cfbf41d6e98496fb29554
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-03-01 17:51:12 +0530

    negative zero

--- 1.introduction/app.js
+++ 1.introduction/app.js
@@ -5,3 +5,14 @@ console.log(++tow)
 // post increment
 let one = 1
 console.log(one++)
+
+var trendRate = -0;
+console.log(trendRate === -0);
+
+console.log(trendRate.toString());
+// console.log(trendRate.toString());
+console.log(trendRate===0);
+console.log(trendRate>0);
+console.log(trendRate<0);
+console.log(Object.is(trendRate,-0));
+console.log(Object.is(trendRate,0));
