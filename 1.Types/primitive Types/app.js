dcc0c17ca76ff788f283dc0834321b006287267d
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-03-13 18:17:20 +0530

    types

---
+++ 1.Types/primitive Types/app.js
@@ -0,0 +1,19 @@
+let num = 10;
+let str = "Hello, world!";
+let bool = true;
+let n = null;
+let undef;
+let sym = Symbol("foo");
+let bigInt = 9007199254740991n;
+
+console.log("Number:", num);
+console.log("String:", str);
+console.log("Boolean:", bool);
+console.log("Null:", n);
+console.log("Undefined:", undef);
+console.log("Symbol:", sym);
+console.log("BigInt:", bigInt);
+
+const sym1 = Symbol('sym2');
+const sym2 = Symbol('sym2');
+console.log(sym1 === sym2); 
---
+++ readme.md
@@ -0,0 +1,1 @@
+# [1.primitive](./markdown/primitve%20types/1.markdown.md)
