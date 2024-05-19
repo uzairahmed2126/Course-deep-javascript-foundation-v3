d81da76ed97b96d46f718a3e17627d0b58bb9e49
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-19 16:45:44 +0530

    bigint

--- 1.Types/3.bigint/app.js
+++ 1.Types/3.bigint/app.js
@@ -1,8 +1,28 @@
-let n = 111111111111112222243;
-console.log(typeof BigInt(n));
+let n = 111111111111112222243n;
+console.log(typeof n);
 
 let bigint = 111111111111112222243n;
 console.log(typeof bigint);
 
-let num = 12n;
+let num = 12;
 console.log(typeof num);
+
+const previouslyMaxSafeInteger = 9007199254740991n;
+console.log(previouslyMaxSafeInteger);
+
+const alsoHuge = BigInt(9007199254740991);
+console.log(alsoHuge); // 9007199254740991n
+
+const hugeString = BigInt("9007199254740991");
+console.log(hugeString); // 9007199254740991n
+
+const hugeHex = BigInt("0x1fffffffffffff");
+console.log(hugeHex); // 9007199254740991n
+
+const hugeOctal = BigInt("0o377777777777777777");
+console.log(hugeOctal); // 9007199254740991n
+
+const hugeBin = BigInt(
+  "0b11111111111111111111111111111111111111111111111111111"
+);
+console.log(hugeBin); // 9007199254740991n
