36c5b74d1a0a251fb2cd977203d6d429a3ffc432
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-19 18:48:07 +0530

    nan

--- 1.Types/5.NaN/app.js
+++ 1.Types/5.NaN/app.js
@@ -1,13 +1,15 @@
-let a = 'asdf';
-console.log(isNaN(a));//true
+let a = "asdf";
+console.log(isNaN(a)); //true
 
 let b = 123;
-console.log(NaN !== b);//true
+console.log(NaN !== b); //true
 
-console.log(isNaN('123'));//false
+console.log(isNaN("123")); //false
 
-const age = Number('0o24');
+const age = Number("0o24");
 console.log(age);
 
-let mycatsAGe = Number('n/a');
-console.log(Number.isNaN(mycatsAGe))
+let mycatsAGe = Number("n/a");
+console.log(Number.isNaN(mycatsAGe));
+
+console.log(false ** false === 1); // true
