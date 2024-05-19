7499d64cbb6eb56527d336fa6ee44f7d72b5a5ee
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-05-19 16:50:46 +0530

    kinds of emptiness

--- 1.Types/4.kinds of emptiness/app.js
+++ 1.Types/4.kinds of emptiness/app.js
@@ -1,4 +1,15 @@
 let x;
-console.log(x);//undefined
+console.log(x); //undefined
 // console.log(a);//ReferenceError: a is not defined
 //const b;//SyntaxError: Missing initializer in const declaration
+try {
+  console.log(a); //ReferenceError: a is not defined
+} catch (e) {
+  console.log("Undeclared variable error:", e.message);
+}
+
+try {
+    console.log(x1); // ReferenceError
+} catch (e) {
+    console.log(e); // ReferenceError: x is not defined
+}
