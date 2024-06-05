57297d9334251234042dc25a5d6638be0f06a907
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-05 18:42:09 +0530

    double equals walkthrough

---
+++ 4.equality/3.Double Equals Walkthrough/app.js
@@ -0,0 +1,5 @@
+console.log(43 == [43]);
+// console.log(43 === [43]);
+console.log(42 == "42"); //coercively it is turning string into number
+console.log(true == "1");
+console.log(true === 1);// in this it's checking value and their type
