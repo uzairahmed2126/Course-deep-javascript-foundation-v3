d1a649edbd6f1ca2d4ca99c94ad3aa65ed3c9f7d
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-06 18:04:59 +0530

    boolean corner cases

---
+++ 4.equality/4.boolean corner cases/app.js
@@ -0,0 +1,8 @@
+const workshopStudent = [];
+console.log(true === true);
+console.log(0 == 1);
+console.log("" === true);
+console.log(false === false);
+console.log(Boolean(workshopStudent));
+console.log(Boolean([]));
+console.log(+[] === 0);
