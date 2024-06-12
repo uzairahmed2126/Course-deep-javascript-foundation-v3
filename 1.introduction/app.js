308850a5d414be5b30c1a87640e44a50eec81b64
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-13 04:04:06 +0530

    Introduction

---
+++ 1.introduction/app.js
@@ -0,0 +1,19 @@
+// let num = 90;
+// num++;
+// console.log(num);
+
+// let num = 24;
+// ++num;
+// console.log(num);
+// let num = 90;
+// console.log(++num);//91
+let num = "90";
+console.log(++num);
+
+let stringNumber = "10";
+console.log(stringNumber++);
+console.log(stringNumber);
+// console.log(+stringNumber);
+// console.log(stringNumber / 1);
+// console.log(stringNumber * 1);
+// console.log(stringNumber - 0);
