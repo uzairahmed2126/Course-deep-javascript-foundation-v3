c4b52ca05bb0442a11154470b0ea976b6a0812fd
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-06-22 18:40:41 +0530

    negative zerro

--- 1.Types/6.negative zero/app.js
+++ 1.Types/6.negative zero/app.js
@@ -1,6 +1,8 @@
 const negatieZero = -0;
 console.log(negatieZero === -0); //it's not a correct answer
 
-console.log(Object.is(negatieZero, -0));//true
+console.log(Object.is(negatieZero, -0)); //true
 
-
+console.log(negatieZero > 0);
+console.log(negatieZero < 0);
+console.log(negatieZero === 0);
