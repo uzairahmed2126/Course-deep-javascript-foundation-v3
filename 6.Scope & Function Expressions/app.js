80de731c1bbacc3b8c3d0765cea52749096031bd
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-09-15 22:25:30 +0530

    function expression

---
+++ 6.Scope & Function Expressions/app.js
@@ -0,0 +1,12 @@
+// function expression are two types
+// 1. named function
+let userName = function userName(name) {
+  return name;
+};
+console.log("john");
+
+// 2. Anonymous function are called function expression
+let userId = function (id) {
+  return id;
+};
+console.log(userId("@#123"));
